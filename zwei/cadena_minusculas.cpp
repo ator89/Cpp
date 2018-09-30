4a80caa5553dff28bddb48459c73a4c2e097b264
Angel Torres <ator89@outlook.com>
2018-09-29 23:07:26 -0600

    Minusculas

---
+++ zwei/cadena_minusculas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  char a[] = "ANGEL";
+  
+  strlwr(a);
+  
+  cout << a << endl;
+  
+  system("pause");
+  return 0;
+}
