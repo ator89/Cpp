71ded4594746322cb4a30e67d8e157d51c3f40de
Angel Torres <ator89@outlook.com>
2018-09-29 18:08:29 -0600

    Adivinar numero

--- zwei/adivinar_num.cpp
+++ zwei/adivinar_num.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
+  int num, dato, ct=0;
+  
+  srand(time(NULL));
+  dato = 1 + rand()%(100);
+  
+  do{
+     cout << "Ingresar un numero: "; cin >> num;
+     
+     if(num>dato)
+     {
+        cout << "\nIngrese un numero menor\n";
+     }
+     if(num>dato)
+     {
+        cout << "\nIngrese un numero mayor";
+     }
+     ct++;
+     
+  }while(num!=dato);
+  
+  cout << "Numero de intentos " << ct << endl;
+  cout << "N intentos " << dato << endl;
     
   system("pause");
   return 0;
