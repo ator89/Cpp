159ecda5f1c7ff196ae66c23c78342e1e01f1839
Angel Torres <ator89@outlook.com>
2018-09-29 21:50:20 -0600

    copiar cadena

---
+++ zwei/cadena_copiar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+  char nombre[] = "Angel";
+  char nombre2[20];
+  
+  strcpy(nombre2,nombre);
+  
+  cout << nombre2<<endl;
+  
+  system("pause");
+  return 0;
+}
