d045e7f84050255cc422d60e9933bc54192a3314
Angel Torres <ator89@outlook.com>
2018-09-29 21:36:40 -0600

    Cadena caracteres

---
+++ zwei/cadena_caracteres.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#inlcude <string.h>
+
+using namespace std;
+
+int main()
+{
+  char word[]= "Angel";
+  char word1[] = {'A','n','g','e','l'};
+    
+  cout<< word <<endl;
+  cout << word1 << endl;
+  
+  return 0;
+}
