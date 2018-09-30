e88853f780252a0ef5075c941704027588bcf1e4
Angel Torres <ator89@outlook.com>
2018-09-29 23:05:20 -0600

    Mayusculas

---
+++ zwei/cadena_mayusculas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+  char a[] = "angel";
+   
+  strupr(a);
+  
+  cout << a << endl;
+    
+  system("pause");
+  return 0;
+}
