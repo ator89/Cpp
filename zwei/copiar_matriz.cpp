98240f1ce072ddf3df339b4668d66234d8e3ff13
Angel Torres <ator89@outlook.com>
2018-09-29 20:53:59 -0600

    copiar matriz 2x2

--- zwei/copiar_matriz.cpp
+++ zwei/copiar_matriz.cpp
@@ -4,7 +4,25 @@ using namespace std;
 
 int main()
 {
-    
+  int num[2][2]={{1,2},{3,4}};  
+  int matriz[2][2];
+  
+  for (int i=0; i<2;i++)
+  {
+      for(int j=0; j<2;j++)
+      {
+         matriz[i][j]= num[i][j];
+      }
+  }
+  
+  for (int i = 0; i<2; i++)
+  {
+      for (int j = 0; j < 2; j++)
+      {
+          cout << matriz[i][j];
+      }
+      cout << endl;
+  }
   
   system("pause");
   return 0;
