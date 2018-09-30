afc3a551862d0227a8f174ad6f5af7a1db2fec0d
Angel Torres <ator89@outlook.com>
2018-09-29 23:33:17 -0600

    Bubble sort

--- zwei/ordenamiento_burbuja.cpp
+++ zwei/ordenamiento_burbuja.cpp
@@ -4,6 +4,29 @@ using namespace std;
 
 int main()
 {
+  int num[] = {5,3,1,2,4};
+  int i,j,aux;
+  
+  //Bubble sort
+  for (i=0;i<5;i++){
+     for(j=0;j<5;j++){
+        if(num[j]>num[j+1]){
+           aux = num[j];
+           num[j]=num[j+1];
+           num[j+1]=aux;
+        }
+     }
+  }
+  
+  cout << "Ascendente: "<<endl;
+  for(i=0;i<5;i++){
+     cout << num[i]<<endl;
+  }
+  
+  cout << "Descendente: "<<endl;
+  for(i=4;i>=0;i--){
+     cout << num[i]<<endl;
+  }
   
     
   system("pause");
