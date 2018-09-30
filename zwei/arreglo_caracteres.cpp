7dcc67dfcf236c0b2af90f3bae690b4de3562958
Angel Torres <ator89@outlook.com>
2018-09-29 18:42:06 -0600

    Arreglo de caracteres

--- zwei/arreglo_caracteres.cpp
+++ zwei/arreglo_caracteres.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
-
+#include <conio.h>
 
 using namespace std;
 
 int main()
 {
-  char letras1[]={'1','2','3','4'};
-  char letras2[]={'t','y','e','g'};
-  char letras[8];
+  char letras1[]={'p','f','m','b','j'};
+  char letras2[]={'t','y','e','g','k'};
+  char letras[10];
   
-  for(int i=0; i<4;i++)
+  for(int i=0; i<5;i++)
   {
      letras[i]=letras1[i];
   }
   
-  for(int i=0; i<8;i++)
+  for(int i=5; i<10;i++)
   {
-     letras[i]=letras2[i-4];
+     letras[i]=letras2[i-5];
   }
   
-  for(int i=0;i<8;i++)
+  for(int i=0;i<10;i++)
   {
      cout << letras[i] << endl;
   }
