fdaaee0b262f826b600d7438f79c462307976f45
Angel Torres <ator89@outlook.com>
2018-09-29 21:09:11 -0600

    Matriz transpuesta

---
+++ zwei/matriz_transpuesta.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int matriz[3][3];
+  int matrizT[3][3];
+  
+  for (int i=0; i<3;i++)
+  {
+      for(int j=0;j<3;j++)
+      {
+         cout << "Digite un numero ["<< i<<"][" <<j<<"]";
+         cin >> matriz[i][j];
+      }
+  }
+  
+  cout << "Matriz original"<<endl;
+  for(int i=0; i<3;i++)
+  {
+     for(int j=0; j<3;j++)
+     {
+        cout << matriz[i][j];
+     }
+     cout << endl;
+  }
+  
+  
+  
+  system("pause");
+  return 0;
+}
