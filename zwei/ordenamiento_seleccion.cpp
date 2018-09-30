0cfa6d1ab3f20bb5a8bc89b203f88f8959d1afdb
Angel Torres <ator89@outlook.com>
2018-09-30 00:02:27 -0600

    Selction sort

--- zwei/ordenamiento_seleccion.cpp
+++ zwei/ordenamiento_seleccion.cpp
@@ -5,8 +5,30 @@ using namespace std;
 int main()
 {
   int num[] = {4,3,1,5,2};
-  int min;
+  int i,j,aux,min;
+  
+  for(i=0;i<5;i++){
+     min = i;
+     for(j=i+1;j<5;j++){
+        if(num[j]<num[min]){
+           min = j;
+        }
+     }
+     aux = num[i];
+     num[i] = num[min];
+     num[min]=aux;
+  }
+  
+  cout << "Ascendente: "<<endl;
+  for(i=0;i<5;i++){
+     cout << num[i]<<endl;
+  }
+  
+  cout << "Descendente: "<<endl;
+  for(i=4;i>=0;i--){
+     cout << num[i]<<endl;
+  }
     
-  system("system");
+  system("pause");
   return 0;
 }
