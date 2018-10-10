b9ee83a65efbc2b30d8bb456b4ffb9707ed61b65
Angel Torres <ator89@outlook.com>
2018-10-10 01:08:46 -0600

    b

---
+++ drei/nnnn.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  
+  vector<string> no;
+  //no.push_back(5);
+  
+  for(int i=0;i<no.size();i++){
+     cout << n[i] << endl;        
+  }
+    
+  system("pause");
+  return 0;
+}
