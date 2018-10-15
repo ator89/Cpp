82e050bc86f7c71276ee3ba97773c612960fa5b4
Angel Torres <ator89@outlook.com>
2018-10-14 22:50:54 -0600

    entero a binario

--- vier/binario.cpp
+++ vier/binario.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
+
+void binario(int n){
+   if(n>1) binario (n/2);
+   
+   cout << n%2;
+}
+
 int main()
 {
+  int n;
+  
+  do{
+     cout << "Ingrese un numberi";
+     cin >> n;
+     
+     if(n<0){
+        cout << "\nNUmero invalido...";
+     }
+  }while(n<0);  
+    
+  cout << "\n numero: "<<n;
+  cout << "\n binario: ";
+  binario(n);
+  getch();
   return 0;
 }
