8ed6d297447ac095d988c846ac4c9ff5c86185ca
Angel Torres <ator89@outlook.com>
2018-10-15 00:47:11 -0600

    Menor de 3 numeros

---
+++ vier/menor_3_numeros.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+  int a, b, c, d, menor=0;
+  
+  for ( int=1 ; i<=3 ; i++ ){
+     cout << "Numero: "<< endl;
+     
+     if(a < menor){
+        menor = a;
+     }else if(b<menor){
+        
+     }
+  }
+  
+  getch();
+  return 0;
+}
