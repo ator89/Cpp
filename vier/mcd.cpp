607213420b419335b4390e34f73f1abc3caf0558
Angel Torres <ator89@outlook.com>
2018-10-14 23:52:38 -0600

    MCD

---
+++ vier/mcd.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+  
+  int a,b;
+  cout << "";
+  cout << "";
+  cout << "MCD de dos numeros" << endl << endl;
+  
+  do{
+  
+  }while(a<0);
+  
+  do{
+     cout << "";
+     cin >> b;
+     
+     if(b<0) cout << "Debe ser positivo...\n";
+  }while (b<0);
+  
+  getch();
+  return 0;
+}
