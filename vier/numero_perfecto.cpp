7d766c325cf08f4190edb21b8e88673c7411d634
Angel Torres <ator89@outlook.com>
2018-10-16 00:25:47 -0600

    Numero Perfecto

--- vier/numero_perfecto.cpp
+++ vier/numero_perfecto.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main()
 {
+  int num = 9;
+  int div = 0;
+  for (int i=1; i<num; i++){
+      if(num % i == 0 ) div+=i;    
+  }
+  if (num ==div ) cout<< num << " es perfecto.";
+  else std::cout << num << " no es perfecto.";
+  
+  system("pause");
   return 0;
 }
