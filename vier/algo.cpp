eb728ca1dbdeab0cdad74a68789e0aee362c9953
Angel Torres <ator89@outlook.com>
2018-10-17 23:49:27 -0600

    algo

---
+++ vier/algo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void arreglo(char[],int);
+
+int main()
+{
+  cout << "Algo" << endl;
+  
+  system("pause");
+  return 0;
+}
+
+void arreglo(char arreglo[],int size){
+   for (int i = 0; i <= size; i++){
+      
+   }  
+   
+}
