8d87adc2fe14445b7f0a21e3ff7f5ef6c746d85e
Angel Torres <ator89@outlook.com>
2018-10-18 08:41:31 -0600

    Tnd

---
+++ funf/menu.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cin;
+using std::coud;
+using std::endl;
+
+#include <string>
+using std::string;
+
+int menu(int);
+
+int main(){
+
+   cout << "Welcome" << endl;
+
+   return 0;
+}
+
+
+int menu(int opcion){
+
+   
+   return opcion;
+}
