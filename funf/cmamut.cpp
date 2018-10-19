d283abb341aa236598c2d901ab3eeb1dd6267f9b
Angel Torres <ator89@outlook.com>
2018-10-19 00:56:08 -0600

    swap sin tercer variable

--- funf/cmamut.cpp
+++ funf/cmamut.cpp
@@ -6,7 +6,10 @@ using std::endl;
 #include <string>
 using std::string;
 
-void swap(int&,int&);
+//swap usando una variable temporal
+void swap1(int&,int&);
+//swap sin usar variable temporal
+void swap2(int&,int&);
 
 void bubble(int[],int);
 
@@ -14,17 +17,44 @@ int main(){
 
    int a = 10;
    int b = 15;
-   
-   swap(a,b);
+   cout << "Primeros valores" << endl;
+   cout << a << " " << b << endl;
 
+   //swap1(a,b);
+
+   cout << "Nuevos valores" <<endl;
+   cout << a << " " << b << endl;
+   swap2(a,b);
+   cout << "Nuevo valores" << endl;
+   cout << a << " " << b << endl;
+   
 
 
    return 0;
 }
 
+//Bubble Sort
 void bubble(int arr[], int size){
-
+   for( int i = 0; i < size; i++ ){
+      for( int j = 0; j < size-1; j++ ){
+         if( arr[i]>arr[j+1]){
+	     swap1(arr[i],arr[j+1]);
+	 }
+      }
+   }
 }
-void swap(int& a, int& b){
-   
+
+//swap usuando una variable temporal
+void swap1(int& a, int& b){
+   int x;
+   x=a;
+   a=b;
+   b=x;
+}
+
+//swap sin usar una variable temporal
+void swap2(int& a, int& b){
+   a = a + b;
+   b = a - b;
+   a = a - b;
 }
