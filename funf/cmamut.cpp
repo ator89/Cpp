8650cfb1ef6f06780e17fec4205726330230284e
Angel Torres <ator89@outlook.com>
2018-10-18 23:43:25 -0600

    new test

---
+++ funf/cmamut.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#include <string>
+using std::string;
+
+void swap(int&,int&);
+
+void bubble(int[],int);
+
+int main(){
+
+   int a = 10;
+   int b = 15;
+   
+   swap(a,b);
+
+
+
+   return 0;
+}
+
+void bubble(int arr[], int size){
+
+}
+void swap(int& a, int& b){
+   
+}
