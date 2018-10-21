48d4052dedf550df7cbb2e44182690b1f9360205
Angel Torres <ator89@outlook.com>
2018-10-20 21:00:27 -0600

    rombo

--- funf/rombo.cpp
+++ funf/rombo.cpp
@@ -16,6 +16,16 @@ int main(){
       }
       cout << '\\' << endl;
    }
+   for (i = 0; i < n; i++ ){
+      for(j = 0; j < i; j++){
+         cout << ' ';
+      }
+      cout << '\\';
+      for(j = 0; j < (n-1-i)*2; j++){
+         cout << ' ';
+      }
+      cout << '/' << endl;
+   }
 
 }
 
