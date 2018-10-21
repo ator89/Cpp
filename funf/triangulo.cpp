7c5ec9445a47f3e87fe199b3c74c3cced05bc564
Angel Torres <ator89@outlook.com>
2018-10-20 20:17:08 -0600

    Es triangulo

--- funf/triangulo.cpp
+++ funf/triangulo.cpp
@@ -10,24 +10,40 @@ int main(){
    int a,b,c;
    cin >> a >> b >> c;
 
-   cout << esTriangulo(a, b, c) << endl;
-
+   if(esTriangulo(a, b, c)){
+      cout << "Es triángulo" << endl;
+   }else{
+      cout << "No es triángulo" << endl;
+   }
    return 0;
 }
 
 bool esTriangulo(double a, double b, double c){
-   int mayor;
+   double mayor;
+   double x1, x2;
 
    if ( a > b && a > c ){
        mayor = a;
+       x1 = b;
+       x2 = c;
    }else if ( b > a && b > c){
        mayor = b;
+       x1 = a;
+       x2 = c;
    }else{
        mayor = c;
+       x1 = a;
+       x2 = b;
    }
 
-   //if ()
+   if( a==b && a==c && b==c ){
+      return false;
+   }else if ( mayor < (x1 + x2)){
+      return true;
+   }else{
+      return false;
+   }
 
-   return false;
+
 
 }
