01885574a555d8a5f81d2ad753725e7c8eba9140
Angel Torres <ator89@outlook.com>
2018-10-20 21:18:09 -0600

    Rombo #2

--- funf/rombo2.cpp
+++ funf/rombo2.cpp
@@ -6,11 +6,31 @@ using std::cout;
 void linea(int espacio, int pos, char c);
 
 int main(){
+   int n, i;
+   cin >> n;
+
+   for( i= 0; i < n; i++){
+      linea(n,n-1-i,'/');
+      linea(n, i, '\\');
+      cout << endl;
+   }
+   for( i= 0; i < n; i++){
+      linea(n,i,'\\');
+      linea(n, n-1-i, '/');
+      cout << endl;
+   }
+
+   /*
    linea(3,2,'/');
    linea(3,0,'\\');
+   cout << endl;
    linea(3,1,'/');
+   linea(3,1,'\\');
+   cout << endl;
+   linea(3,0,'/');
    linea(3,2,'\\');
-   //cout << '.' << endl;
+   cout << endl;
+   //cout << '.' << endl;*/
    return 0;
 }
 
