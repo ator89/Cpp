851cbeb96fd427f167fa29d30027fd83a41930be
Angel Torres <ator89@outlook.com>
2018-10-20 19:25:59 -0600

    loop

---
+++ funf/n_3cifras.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   int i;
+   for ( i = 100; i <= 999; i++ ){
+       int c, d, u;
+
+       if(c + d == u){
+           cout << i << ' ' ;
+       }
+   }
+
+   return 0;
+}
