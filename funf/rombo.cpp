62bbc9877b512479bf1279e4566210b6eeaf21d9
Angel Torres <ator89@outlook.com>
2018-10-20 20:49:24 -0600

    rombo

---
+++ funf/rombo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::cin;
+using std::endl;
+using std::cout;
+
+int main(){
+   int n, i, j;
+   cin >> n;
+   for (i = 0; i < n; i++ ){
+      for(j = 0; j < (n - 1 - i); j++){
+         cout << ' ';
+      }
+      cout << '/';
+      for(j = 0; j < i*2; j++){
+         cout << ' ';
+      }
+      cout << '\\' << endl;
+   }
+
+}
+
