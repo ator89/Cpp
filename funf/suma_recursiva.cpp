5a14fe70f2e87380b7a204ae30235444694c910f
Angel Torres <ator89@outlook.com>
2018-10-20 22:19:22 -0600

    recursividad

---
+++ funf/suma_recursiva.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int suma(int);
+
+int main(){
+
+   cout << suma(5)<< endl;
+
+   return 0;
+}
+
+int suma(int n){
+   if(n <= 0) return 0;
+   return n + suma(n-1);
+}
