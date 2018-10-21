50888f99cd80751cf92f316943567e944e355675
Angel Torres <ator89@outlook.com>
2018-10-20 19:30:05 -0600

    suma cifras

--- funf/n_3cifras.cpp
+++ funf/n_3cifras.cpp
@@ -7,6 +7,10 @@ int main(){
    for ( i = 100; i <= 999; i++ ){
        int c, d, u;
 
+       c = i / 100;
+       d = ( i / 10 ) % 10;
+       u = i % 10;
+
        if(c + d == u){
            cout << i << ' ' ;
        }
