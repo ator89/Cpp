2f90b7faef755f353a957e8df763f58a1d3832df
Angel Torres <ator89@outlook.com>
2018-10-20 21:49:42 -0600

    serie creciente

---
+++ funf/serie_creciente.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+   int n, last;
+   cin >> n;
+
+   while( n! = 0){
+      last = n;
+      cin >> n;
+   }
+
+   return 0;
+}
