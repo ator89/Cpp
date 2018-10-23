4a971ee9bcd379b3f3d6e2ca1a420702350c22da
Angel Torres <ator89@outlook.com>
2018-10-23 15:11:56 -0600

    Producto

---
+++ funf/producto.cpp
@@ -0,0 +1,23 @@
+#inlcude <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+struct numero{
+   int cifras[30];
+   int ncifras;
+};
+
+int main(){
+   int s; //4 bytes = 32 bits
+   Numero N;
+   int t, tam;
+   tam = s.size;
+
+   for( i = 0; i < tam < i++){
+      char digito = s[i];
+   }
+
+   return 0;
+}
