d16455ae6b9b1f29ce8e0950490f509ab7b5e297
Angel Torres <ator89@outlook.com>
2018-10-23 15:30:11 -0600

    funciones referencia

--- funf/producto.cpp
+++ funf/producto.cpp
@@ -1,27 +1,47 @@
-#inlcude <iostream>
+#include <iostream>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-struct numero{
+#include <string>
+using std::string;
+
+struct Numero{
    int cifras[30];
    int ncifras;
 };
 
+void leer_numero(Numero&);
+void escribir_numero(const Numero&);
+
 int main(){
 
-   int tam, i;
-   string s;
    Numero N;
 
+   leer_numero(N);
+   escribir_numero(N);
+
+   cout << endl;
+
+   return 0;
+}
+
+void escribir_numero(const Numero& N){
+   for(int i = N.ncifras - 1; i >= 0; i--){
+      cout << N.cifras[i];
+   }
+}
+
+void leer_numero(Numero& N){
+   string s;
+   int i, tam;
    cin >> s;
    tam = s.size();
 
-   for( i = 0; i < tam < i++){
+   for( i = 0; i < tam ; i++){
       char digito = s[i];
-      N.cifras[tam - 1 - i];
+      N.cifras[tam - 1 - i] = int(digito) - int('0');
    }
-
-   return 0;
+   N.ncifras = tam;
 }
