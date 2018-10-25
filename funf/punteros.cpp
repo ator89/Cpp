797b745d588a0896b31604fc9e12233ee0f2c4bf
Angel Torres <ator89@outlook.com>
2018-10-24 22:49:27 -0600

    punteros

--- funf/punteros.cpp
+++ funf/punteros.cpp
@@ -7,8 +7,19 @@ int main(){
     int t[5];
 
     for( int i = 0; i < 5; i++ ){
-        cout << &t[i] << endl;
+        cout << int(&t[i]) << endl;
     }
+
+    cout << endl << endl;
+
+    double T[5] = {1.0,2.0,3.0,4.0,5.0};
+    double *p;
+    p = &T[0];
+    //*(p+2) = -5.0;
+    //T[2]
+    p = p +1;
+    cout << *(p+1) << endl;
+
 
     return 0;
 }
